4c2c3221dca6d0792259919789e4201e1fab86b5
Magnus Landerblom <magnus@landerblom.se>
2017-11-15 08:36:03 +0100

    Consumer and Producer

---
+++ consumer.py
@@ -0,0 +1,49 @@
+import sys
+import os
+
+from confluent_kafka import Consumer, KafkaException, KafkaError
+
+if __name__ == '__main__':
+    topics = ["%s.test" % os.environ['CLOUDKARAFKA_TOPIC_PREFIX']]
+
+    # Consumer configuration
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    conf = {
+        'bootstrap.servers': os.environ['CLOUDKARAFKA_BROKERS'],
+        'group.id': "cloudkarafka-example",
+        'session.timeout.ms': 6000,
+        'default.topic.config': {'auto.offset.reset': 'smallest'},
+        'security.protocol': 'SASL_SSL',
+	'sasl.mechanisms': 'SCRAM-SHA-256',
+        'sasl.username': os.environ['CLOUDKARAFKA_USERNAME'],
+        'sasl.password': os.environ['CLOUDKARAFKA_PASSWORD']
+    }
+
+    c = Consumer(**conf)
+    c.subscribe(topics)
+    try:
+        while True:
+            msg = c.poll(timeout=1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                # Error or event
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    # End of partition event
+                    sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
+                                     (msg.topic(), msg.partition(), msg.offset()))
+                elif msg.error():
+                    # Error
+                    raise KafkaException(msg.error())
+            else:
+                # Proper message
+                sys.stderr.write('%% %s [%d] at offset %d with key %s:\n' %
+                                 (msg.topic(), msg.partition(), msg.offset(),
+                                  str(msg.key())))
+                print(msg.value())
+
+    except KeyboardInterrupt:
+        sys.stderr.write('%% Aborted by user\n')
+
+    # Close down consumer to commit final offsets.
+    c.close()
---
+++ producer.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+from confluent_kafka import Producer
+
+if __name__ == '__main__':
+    topic = "%s.test" % os.environ['CLOUDKARAFKA_TOPIC_PREFIX']
+
+    # Consumer configuration
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    conf = {
+        'bootstrap.servers': os.environ['CLOUDKARAFKA_BROKERS'],
+        'session.timeout.ms': 6000,
+        'default.topic.config': {'auto.offset.reset': 'smallest'},
+        'security.protocol': 'SASL_SSL',
+	'sasl.mechanisms': 'SCRAM-SHA-256',
+        'sasl.username': os.environ['CLOUDKARAFKA_USERNAME'],
+        'sasl.password': os.environ['CLOUDKARAFKA_PASSWORD']
+    }
+
+    p = Producer(**conf)
+
+    def delivery_callback(err, msg):
+        if err:
+            sys.stderr.write('%% Message failed delivery: %s\n' % err)
+        else:
+            sys.stderr.write('%% Message delivered to %s [%d]\n' %
+                             (msg.topic(), msg.partition()))
+
+    for line in sys.stdin:
+        try:
+            p.produce(topic, line.rstrip(), callback=delivery_callback)
+        except BufferError as e:
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
+                             len(p))
+        p.poll(0)
+
+    sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
+    p.flush()
