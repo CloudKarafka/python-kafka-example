2a1424d6762fc4be8fd557cc07a8c3ac3b509ce5
SANJANA KRITHI <33476803+sanjanakrithi@users.noreply.github.com>
2019-07-25 12:20:38 -0400

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -4,7 +4,7 @@ import os
 from confluent_kafka import Consumer, KafkaException, KafkaError
 
 if __name__ == '__main__':
-    topics = os.environ['CLOUDKARAFKA_TOPIC'].split(",")
+    topics = os.environ['CLOUDKARAFKA_TOPIC_PREFIX'].split(",")
 
     # Consumer configuration
     # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
